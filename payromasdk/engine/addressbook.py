009ee6a59bb9a6e5cc495373f30e02261ca14062
Payroma Developer <payromafinance@gmail.com>
2021-12-03 21:34:43 +0200

    Added address book engine

--- payromasdk/engine/addressbook.py
+++ payromasdk/engine/addressbook.py
@@ -3,12 +3,38 @@ from ..data import addressesbook
 
 
 def get_all() -> list:
-    pass
+    return list(addressesbook.db.get_data().values())
 
 
-def add_new(address_book_interface: interface.AddressBook) -> bool:
-    pass
+def add_new(username: str, address: interface.Address) -> bool:
+    """
+    Add a new address book
+    :exception SPDatabase.ITEM_EXISTS_ERROR
+    :exception OSError, FileNotFoundError, PermissionError
+    :return: bool
+    """
+
+    address_book_interface = interface.AddressBook(
+        username=username, address=address
+    )
+    addressesbook.db.update_item(value=address_book_interface, item_id=address_book_interface.addressID)
+
+    return True
 
 
 def remove(address_book_interface: interface.AddressBook) -> bool:
-    pass
+    """
+    Remove specific address book interface
+    :return: bool
+    """
+
+    valid = False
+    if isinstance(address_book_interface, interface.AddressBook):
+        try:
+            addressesbook.db.remove_item(item_id=address_book_interface.addressID)
+        except KeyError:
+            pass
+        else:
+            valid = True
+
+    return valid
