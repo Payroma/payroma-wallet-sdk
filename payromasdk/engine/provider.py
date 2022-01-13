6f43a728146292f5c3f20c115c835ee4208d54d6
Payroma Developer <payromafinance@gmail.com>
2022-01-13 11:14:27 +0200

    EIP1559 supported

--- payromasdk/engine/provider.py
+++ payromasdk/engine/provider.py
@@ -15,6 +15,16 @@ class Metadata:
     CHAIN_ID = 'chainId'
 
 
+class EIP1559Metadata:
+    SLOW = 'slow'
+    MEDIUM = 'medium'
+    FAST = 'fast'
+    BLOCK_NUMBER = 'blockNumber'
+    BASE_FEE_PER_GAS = 'baseFeePerGas'
+    GAS_USED_RATIO = 'gasUsedRatio'
+    PRIORITY_FEE_PER_GAS = 'priorityFeePerGas'
+
+
 class __Provider(object):
     def __init__(self):
         self.__contract = None
@@ -47,19 +57,27 @@ class __Provider(object):
             Metadata.CHAIN_ID: self.interface.chainID
         }
 
-    def add_gas(self, tx_data: dict, eth_eip1559: bool = False):
+    def add_gas(
+            self, tx_data: dict, eip1559_enabled: bool = False, rate: str = EIP1559Metadata.MEDIUM
+    ) -> interface.WeiAmount:
         gas_limit = self.web3.eth.estimate_gas(tx_data)
 
-        if eth_eip1559 and 'ethereum' in self.interface.name.lower():
-            pass
+        if eip1559_enabled:
+            tx_data.update(
+                self.__estimate_gas_eip1559()[rate]
+            )
+            price = gas_limit * tx_data[Metadata.MAX_FEE_PER_GAS]
         else:
             tx_data.update({
                 Metadata.GAS_PRICE: self.web3.eth.gas_price
             })
+            price = gas_limit * tx_data[Metadata.GAS_PRICE]
 
         tx_data.update({
             Metadata.GAS: gas_limit
         })
+
+        return interface.WeiAmount(price, decimals=18)
 
     def send_transaction(self, tx_data: dict, private_key: str) -> interface.TXHash:
         signed_txn = self.web3.eth.account.sign_transaction(tx_data, private_key=private_key)
@@ -77,6 +95,43 @@ class __Provider(object):
             return self.web3.eth.get_transaction_receipt(transaction_hash.value())
         except exceptions.TransactionNotFound:
             return datastructures.AttributeDict(dict())
+
+    def __fee_history(self) -> list:
+        historical_blocks = 4
+        fee_history = self.web3.eth.fee_history(historical_blocks, 'pending', [25, 50, 75])
+        oldest_block = fee_history.oldestBlock
+        latest_block = oldest_block + historical_blocks
+
+        return [
+            {
+                EIP1559Metadata.BLOCK_NUMBER: block_number,
+                EIP1559Metadata.BASE_FEE_PER_GAS: fee_history.baseFeePerGas[index],
+                EIP1559Metadata.GAS_USED_RATIO: fee_history.gasUsedRatio[index],
+                EIP1559Metadata.PRIORITY_FEE_PER_GAS: fee_history.reward[index]
+            } for index, block_number in enumerate(range(oldest_block, latest_block))
+        ]
+
+    def __estimate_gas_eip1559(self) -> dict:
+        rates = [EIP1559Metadata.SLOW, EIP1559Metadata.MEDIUM, EIP1559Metadata.FAST]
+        fee_history = self.__fee_history()
+        base_fee = self.web3.eth.get_block('pending').baseFeePerGas
+        result = {}
+
+        for index, rate in enumerate(rates):
+            all_priority_fee = [i[EIP1559Metadata.PRIORITY_FEE_PER_GAS][index] for i in fee_history]
+            priority_fee = max(all_priority_fee)
+            max_fee = 2 * base_fee + priority_fee
+            if priority_fee >= max_fee or priority_fee <= 0:
+                raise ValueError("Max fee must exceed the priority fee")
+
+            result.update({
+                rate: {
+                    Metadata.MAX_PRIORITY_FEE_PER_GAS: priority_fee,
+                    Metadata.MAX_FEE_PER_GAS: max_fee,
+                }
+            })
+
+        return result
 
 
 class _MainProvider(__Provider):
